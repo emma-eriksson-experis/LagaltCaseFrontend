2c2ea8a789b413f7dff563b65b27078a4e7e68a9
EEriks <emma.eriksson@se.experis.com>
2023-09-04 19:00:50 +0200

    Created a header and a login modal

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import { LandingPage } from "./routes/LandingPage";
 import { AdminPage } from "./routes/AdminPage";
 import { ProfilePage } from "./routes/ProfilePage";
 import { ProjectPage } from "./routes/ProjectPage";
+import { Header } from "./components/Header";
+import { LoginModal } from "./components/LoginModal";
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
 
 function App() {
+
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
+
   return (
-    <BrowserRouter>
-      <div className="h-screen bg-slate-200">
-        <Routes>
-          <Route path="/" element={ <LandingPage />} />
-          <Route path="/admin" element={ <AdminPage />} />
-          <Route path="/profile/:userId" element={ <ProfilePage />} />
-          <Route path="/project/:projectId" element={ <ProjectPage />} />
-        </Routes>
-      </div>
-   </BrowserRouter>
+    <AppContext.Provider value={{ loginModalOpen, setLoginModalOpen }}>
+      <BrowserRouter>
+        <div className="h-screen bg-slate-200">
+          <Header />
+          <Routes>
+            <Route path="/" element={ <LandingPage />} />
+            <Route path="/admin" element={ <AdminPage />} />
+            <Route path="/profile/:userId" element={ <ProfilePage />} />
+            <Route path="/project/:projectId" element={ <ProjectPage />} />
+          </Routes>
+          <LoginModal open={loginModalOpen} />
+        </div>
+    </BrowserRouter>
+   </AppContext.Provider>
   );
 }
 
