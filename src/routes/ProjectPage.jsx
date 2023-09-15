f9f4a796b8a6b3b0e3bb5ea637d0861a9f3ccf39
EEriks <emma.eriksson@se.experis.com>
2023-09-15 20:19:33 +0200

    Feat: Styled profile page and project page so it looked more consistent fixed navigation on the logotype inside header to landing page

--- src/routes/ProjectPage.jsx
+++ src/routes/ProjectPage.jsx
@@ -155,7 +155,7 @@ export function ProjectPage(){
                                     Close
                                 </button>
                                 <button
-                                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    className="bg-gradient-to-r from-teal-300 to-blue-500 text-white text-white font-normal active:bg-emerald-600 hover:bg-emerald-300 hover:from-blue-500 hover:to-teal-200 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                     type="submit"
                                 >
                                     Submit
