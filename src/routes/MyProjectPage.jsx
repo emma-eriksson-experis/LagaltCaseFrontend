10466a7cad3d04a0a9b795d4a268a6cb4572afdc
EEriks <emma.eriksson@se.experis.com>
2023-09-19 10:05:11 +0200

    feat: fixed my project page

--- src/routes/MyProjectPage.jsx
+++ src/routes/MyProjectPage.jsx
@@ -1,21 +1,24 @@
+import { AppContext } from "../App";
 import { MyProjectBanner } from "../components/MyProjectBanner";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export function MyProjectPage(){
 
     const [projects, setProjects] = useState([]);
-    const [user, setUser] = useState([]);
+    const { user } = useContext(AppContext);
 
     useEffect(() => {
         const fetchProjects = async () => {
-            const response = await fetch(`https://localhost:7291/api/${user.id}/Project/users`);
+            const response = await fetch(`https://localhost:7291/api/Project/${user.id}/users`);
             //Replace endpoint with get all projects by user from user project.
             const data = await response.json();
             setProjects(data);
         }
 
-        fetchProjects();
-    }, []);
+        if (user) {
+            fetchProjects();
+        }
+    }, [user]);
 
     return(
         <main className="flex flex-col items-center">
